ee7474b1844514e2cd9e41c7cc05a09fd423cb22
MarcDK <MarcDK@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-15 12:48:25 +0000

    final tag
    git-svn-id: https://plugins.svn.wordpress.org/marctv-jquery-video-embed/tags/1.6.1@323151 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- marctv_jq_video_embed.php
+++ marctv_jq_video_embed.php
@@ -4,26 +4,26 @@
   Plugin Name: MarcTV jQuery Video Embed
   Plugin URI: http://www.marctv.de/blog/2010/08/25/marctv-wordpress-plugins/
   Description: Embed youtube, vimeo and google videos by just adding a css class to a link which points to the url of a video page.
-  Version: 1.1
+  Version: 1.6.1
   Author: Marc Tönsing
   Author URI: http://www.marctv.de
   License: GPL2
  */
 if (!is_admin()) {
   wp_enqueue_style(
-          "jquery.marctv_video", WP_PLUGIN_URL . "/marctv-jquery-video/jquery.marctv_video.css",
+          "jquery.marctv_video", WP_PLUGIN_URL . "/marctv-jquery-video-embed/jquery.marctv_video.css",
           false, "1.0");
 
   wp_enqueue_script(
-          "toolbox.flashembed", WP_PLUGIN_URL . "/marctv-jquery-video/toolbox.flashembed.min.js",
+          "toolbox.flashembed", WP_PLUGIN_URL . "/marctv-jquery-video-embed/toolbox.flashembed.min.js",
           array("jquery"), "1.2.5", 0);
 
   wp_enqueue_script(
-          "jquery.marctv_video", WP_PLUGIN_URL . "/marctv-jquery-video/jquery.marctv_video.js",
+          "jquery.marctv_video", WP_PLUGIN_URL . "/marctv-jquery-video-embed/jquery.marctv_video.js",
           array("jquery", "toolbox.flashembed"), "1.0", 0);
 
   wp_enqueue_script(
-          "jquery.marctv_videosetup", WP_PLUGIN_URL . "/marctv-jquery-video/jquery.marctv_video_setup.js",
+          "jquery.marctv_videosetup", WP_PLUGIN_URL . "/marctv-jquery-video-embed/jquery.marctv_video_setup.js",
           array("jquery", "jquery.marctv_video", "toolbox.flashembed"), "", 1);
 }
 ?>
